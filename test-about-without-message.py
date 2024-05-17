18c3d760c8a31c516da985ad42b64df6d7000b81
SONALI SRIVASTAVA <srivastava.sonali1@gmail.com>
2024-05-17 18:38:37 +0530

    Update test-about-without-message.py

--- test-about-without-message.py
+++ test-about-without-message.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from chromedriver_py import binary_path # this will get you the path variable
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Chrome(options=options)
+svc = webdriver.ChromeService(executable_path=binary_path)
+driver = webdriver.Chrome(service=svc)
 # Navigate to my blog
 driver.get("https://cerebro1.github.io/")
 # Click on the About button
