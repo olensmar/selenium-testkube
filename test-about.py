4e57ef807100ecf8909c05005c16195be88cb06c
SONALI SRIVASTAVA <srivastava.sonali1@gmail.com>
2024-05-17 13:47:57 +0530

    Test to check About button is clickable

---
+++ test-about.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+print("Test Execution Started")
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+driver = webdriver.Chrome(options=options)
+# Maximize the window size
+driver.maximize_window()
+print("Timer1")
+time.sleep(5)
+# Navigate to my blog
+driver.get("https://cerebro1.github.io/")
+print("Get Successful")
+time.sleep(5)
+# Click on the About button
+about = driver.find_element(By.LINK_TEXT, 'About')
+about.click()
+print("Click Successful")
+time.sleep(5)
+# Close the browser
+driver.close()
+driver.quit()
+print("Test Execution Successfully Completed!")
